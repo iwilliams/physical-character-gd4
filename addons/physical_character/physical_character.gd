a2539949f339d7cfe20df052f03276092f58ff73
Ian Williams <ian@iwillia.ms>
2022-10-01 16:06:13 -0600

    cleanup

--- addons/physical_character/physical_character.gd
+++ addons/physical_character/physical_character.gd
@@ -5,17 +5,17 @@ var ride_spring_strength = 800.0
 var ride_spring_damper = 100.0
 var m_goal_vel = Vector3.ZERO
 var speed_factor = Vector3(1, 0, 1)
-
+var jump_frames = 0
 func _ready():
 	pass # Replace with function body.
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D):
 	var delta = state.step
-#	($ShapeCast3d as ShapeCast3D).force_shapecast_update()
+
 
 	is_on_floor = $ShapeCast3d.is_colliding()
-	var is_on_slope = false
+
 	var floor_normal = Vector3.UP
 	if is_on_floor:
 		var collision_safe_fraction = $ShapeCast3d.get_closest_collision_safe_fraction()
@@ -30,7 +30,7 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 		var other_dir_velcoity = ray_dir.dot(other_velocity)
 
 		var rel_velocity = ray_dir_velocity - other_dir_velcoity
-
+		
 		var ride_height = .5
 		var ride_hit_distance = (1 * collision_safe_fraction) - ride_height
 		var x = ride_hit_distance
@@ -41,23 +41,18 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 		var collision_angle = collision_normal.angle_to(Vector3.UP)
 		
 		constant_force = Vector3.ZERO
-		
-#		if collision_angle >= .5:
-#			is_on_slope = true
+		# Gravity
 		constant_force += (Vector3.DOWN * 9.8 * mass).slide(collision_normal)
-#			constant_force += (Vector3.DOWN * 9.8 * mass).slide(collision_normal)
-
+		# Hover force
 		constant_force += (Vector3.DOWN * spring_force)
 	else:
 		constant_force = Vector3.DOWN * 9.8 * mass
+	
 	var input_vector = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-#	constant_force += Vector3.BACK * input_vector.y * 10
-#	constant_force += Vector3.RIGHT * input_vector.x * 10
 
-
-# desired direction
+	# desired direction
 	var m_unit_goal = Vector3(input_vector.x, 0, input_vector.y)
-	var acceleration = 8.0 if is_on_floor else 0.2
+	var acceleration = 8.0 if is_on_floor else 5.0
 	var max_speed = 3.0
 	var max_acceleration_force = 10.0
 
@@ -65,9 +60,10 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 	var unit_vel = m_goal_vel.normalized()
 	# difference between desired direction and current direction
 	var vel_dot = m_unit_goal.dot(unit_vel)
-
 #	var accel = acceleration * ((settings.acceleration_factor_from_dot as Curve).interpolate(vel_dot) + 1 / 2)
 	var accel = acceleration
+	if vel_dot == -1:
+		accel = accel*4
 	var goal_vel = m_unit_goal * max_speed * speed_factor
 
 	m_goal_vel = m_goal_vel.move_toward(goal_vel, accel * delta)
@@ -78,3 +74,4 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 	needed_accel = needed_accel.normalized() * min(needed_accel.length(), max_accell.length())
 	needed_accel.y = 0.0
 	constant_force += (needed_accel * mass).slide(floor_normal)
+#	constant_force += needed_accel * mass
